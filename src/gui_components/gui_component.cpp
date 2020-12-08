1d1ecefde3275475d332bf6d18503c431ce1d44f
Vincent Eurasto <vincent.eurasto1@gmail.com>
2020-12-08 13:26:43 +0200

    Added unit labels to components

--- src/gui_components/gui_component.cpp
+++ src/gui_components/gui_component.cpp
@@ -58,9 +58,27 @@ void GUIComponent::DrawInfo(sf::RenderWindow &window) {
     if (font.loadFromFile("../fonts/arial.ttf"))
     {
         sf::Text name(GetName(), font, 14);
-        sf::Text value(std::to_string(GetValue()), font, 14);
+        std::string unit;
+        switch (GetType()) {
+            case RESISTOR:
+                unit = " OHM";
+                break;
+            case CAPACITOR:
+                unit = " F";
+                break;
+            case INDUCTOR:
+                unit = " H";
+                break;
+            case VOLTAGE_SOURCE:
+                unit = " V";
+                break;
+            case CURRENT_SOURCE:
+                unit = " A";
+                break;
+        }
+        sf::Text value(std::to_string(GetValue()) + unit, font, 14);
         name.setFillColor(sf::Color::Black);
-        value.setFillColor(sf::Color::Black);
+        value.setFillColor(sf::Color::Yellow);
         auto rot = getRotation();
         auto bounds = getGlobalBounds();
         if ( rot == 90.f || rot == 270.f) {
